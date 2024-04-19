e515df8053c411d00ecb6bc673e70dfd6f2c72b9
Cornelius Monahan <corneliusmonahann@gmail.com>
2024-04-19 12:13:34 -0400

    added rest

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template, jsonify, Flask, request, redirect, url_for
-from app import app
+from app import app, db
 import requests
 import sys
 from app import user_prediction as upd
+
+#ADDED AS AN EXAMPLE FOR THE DUMMY DATA
+from app import make_accounts_example as mae
+from app.models import Accounts
 
 @app.route('/')
 def index():
@@ -35,3 +39,16 @@ def signup_page():
 def predictions():
     gameDate,teams,cvpPrediction=upd.__getPredictedGames()
     return render_template('prediction-page.html',gameDate=gameDate,teams=teams,cvpPrediction=cvpPrediction)
+
+#ALL OF THIS CODE IS JUST TO POPULATE DB WITH EXAMPLE ACCS - THIS ROUTE (and all files) WILL BE REMOVED
+acc_obj = mae.MakeAccounts()
+@app.route('/pop_accounts')
+def pop_accounts(acc_obj = acc_obj):
+    if acc_obj.num_calls <= 0: #will only be called once every time you run the flask app
+        db.drop_all()
+        db.create_all()
+        acc_obj.populate_accounts() #this creates the fake accounts to use for now, and increments 'num_calls'
+        print('DATABASE REFRESHED - EXAMPLE ACCOUNTS CREATED')
+
+    all_accounts = db.session.query(Accounts).all()
+    return render_template('pop_accounts_example.html', accounts = all_accounts)
