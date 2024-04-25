ad3ec5201790a407aa70f94f336f2f724be37cb5
Gavin Newell <grex326@gmail.com>
2024-04-24 21:59:54 -0400

    Added form for taking sign-up info

--- app/forms.py
+++ app/forms.py
@@ -2,3 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
+class signUpForm(FlaskForm):
+    user_name = StringField('Username', validators = [DataRequired()])
+    email = StringField('Email', validators = [DataRequired()])
+    password = StringField('Password', validators = [DataRequired()])
