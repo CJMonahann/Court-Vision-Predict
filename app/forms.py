838aea7b6a48dcb2cbd78b51766ed1b89d2a7fc1
Cornelius Monahan <corneliusmonahann@gmail.com>
2024-05-05 18:23:06 -0400

    changes password field to hide text input and created generic account in db

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 class signUpForm(FlaskForm):
     user_name = StringField('Username', validators = [DataRequired()])
     email = StringField('Email', validators = [DataRequired()])
-    password = StringField('Password', validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
